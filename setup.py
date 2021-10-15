a407f43154e4db954c1aa19ee46f3a2e3c46ce88
LorenzoFederici <lorenzo.federici@uniroma1.it>
2021-10-15 11:59:55 +0200

    added tensorboardx dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(name='pyrlprob',
-    version='1.1.5',
+    version='1.1.6',
     author='Lorenzo Federici',
     author_email = 'federicilorenzo94@gmail.com',
     description = 'Train easily OpenAI-Gym environments through Ray-RLlib',
