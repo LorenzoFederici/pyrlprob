e4afed4a1b64f4efff49f2bc8210f4c4f992bb4f
LorenzoFederici <lorenzo.federici@uniroma1.it>
2022-11-23 18:01:42 -0700

    fixed bug in evaluation

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(name='pyrlprob',
-    version='1.8.5',
+    version='1.8.6',
     author='Lorenzo Federici',
     author_email = 'federicilorenzo94@gmail.com',
     description = 'Train easily Gym-derived environments in python/C++ through Ray RLlib',
