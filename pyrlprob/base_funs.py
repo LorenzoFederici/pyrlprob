8b09487f60df80ead91e52a1feed9b33c29cc9dc
LorenzoFederici <lorenzo.federici@uniroma1.it>
2025-12-03 12:28:48 -0700

    new updaate for eval

--- pyrlprob/base_funs.py
+++ pyrlprob/base_funs.py
@@ -244,24 +244,29 @@ def evaluation(trainer: Union[str, Callable, Type],
 
     # Evaluation and callbacks config
     config["create_env_on_driver"] = False
-    config["evaluation_interval"] = 1
-    config["evaluation_num_workers"] = max(config["evaluation_num_workers"], 1)
-    if evaluation_num_episodes % config["num_envs_per_worker"]:
-        if evaluation_num_episodes % config["evaluation_num_workers"]:
-            config["evaluation_num_episodes"] = evaluation_num_episodes
-            config["num_envs_per_worker"] = 1
-            config["evaluation_num_workers"] = 1
-        else:
-            config["evaluation_num_episodes"] = evaluation_num_episodes
-            config["num_envs_per_worker"] = 1
-    else:
-        if evaluation_num_episodes % config["evaluation_num_workers"]:
-            config["evaluation_num_episodes"] = int(evaluation_num_episodes/config["num_envs_per_worker"])
-            config["evaluation_num_workers"] = 1
-        else:
-            config["evaluation_num_episodes"] = int(evaluation_num_episodes/config["num_envs_per_worker"])
-        
     config["evaluation_config"] = evaluation_config
+    config["evaluation_interval"] = 1
+    if "prng_seed" in evaluation_config:
+        config["evaluation_num_episodes"] = evaluation_num_episodes
+        config["num_envs_per_worker"] = 1
+        config["evaluation_num_workers"] = 1
+    else:
+        config["evaluation_num_workers"] = max(config["evaluation_num_workers"], 1)
+        if evaluation_num_episodes % config["num_envs_per_worker"]:
+            if evaluation_num_episodes % config["evaluation_num_workers"]:
+                config["evaluation_num_episodes"] = evaluation_num_episodes
+                config["num_envs_per_worker"] = 1
+                config["evaluation_num_workers"] = 1
+            else:
+                config["evaluation_num_episodes"] = evaluation_num_episodes
+                config["num_envs_per_worker"] = 1
+        else:
+            if evaluation_num_episodes % config["evaluation_num_workers"]:
+                config["evaluation_num_episodes"] = int(evaluation_num_episodes/config["num_envs_per_worker"])
+                config["evaluation_num_workers"] = 1
+            else:
+                config["evaluation_num_episodes"] = int(evaluation_num_episodes/config["num_envs_per_worker"])
+        
     if custom_eval_function is not None:
         if callable(custom_eval_function):
                 config["custom_eval_function"] = custom_eval_function
